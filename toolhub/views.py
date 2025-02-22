cd2d4bcb27981c70e87660d3f8ec5555b30bc363
PiShrestha <pratikstha980@gmail.com>
2025-02-21 23:43:13 -0500

    Fix librarian redirect

--- toolhub/views.py
+++ toolhub/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(request):
-    if request.user.is_staff:
+    if request.user.role == 'librarian':
         return render(request, "toolhub/librarian_home.html", {"user": request.user})
     return render(request, "toolhub/patron_home.html", {"user": request.user})
 
