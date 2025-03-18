60afcdd3945c4fe75e963c94fa4ed78822d80c61
PiShrestha <pratikstha980@gmail.com>
2025-03-18 05:41:02 -0400

    Fix profile picture delete issue

--- toolhub/models.py
+++ toolhub/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.core.files.storage import default_storage
+from django.utils.module_loading import import_string
 
 def upload_to_profile(instance, filename):
     extension = filename.split('.')[-1]
@@ -15,25 +17,32 @@ class CustomUser(AbstractUser):
 
     phone_number = models.CharField(max_length=15, blank=True, null=True)
 
+    @staticmethod
     def default_profile_picture():
         return "toolhub/images/default.png"
     
     profile_picture = models.ImageField(
         upload_to=upload_to_profile,
-        default= default_profile_picture,
-        storage=settings.DEFAULT_FILE_STORAGE,
-        null= True, 
-        blank= True
-        )
+        default=default_profile_picture,
+        storage=import_string(settings.DEFAULT_FILE_STORAGE)(),
+        null=True, 
+        blank=True
+    )
     
     # Deletes old profile picture before saving a new one.
     def save(self, *args, **kwargs):
         if self.pk:
             try:
                 old_instance = CustomUser.objects.get(pk=self.pk)
-                if old_instance.profile_picture and old_instance.profile_picture.name != self.profile_picture.name:
-                    if hasattr(old_instance.profile_picture, 'delete'):
-                        old_instance.profile_picture.delete(save=False)
+                default_picture = CustomUser.default_profile_picture()
+                # If an old profile picture exists, is not the default, and has changed,
+                # delete the old file from storage.
+                if (old_instance.profile_picture and 
+                    old_instance.profile_picture.name != default_picture and 
+                    old_instance.profile_picture != self.profile_picture):
+                    if old_instance.profile_picture.name:
+                        if default_storage.exists(old_instance.profile_picture.name):
+                            default_storage.delete(old_instance.profile_picture.name)
             except CustomUser.DoesNotExist:
                 pass  # First-time save, no old image exists
         super().save(*args, **kwargs)
